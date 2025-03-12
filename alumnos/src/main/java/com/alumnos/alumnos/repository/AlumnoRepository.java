d27b8d23d5816e9924c4d8dfc170051333159176
LuzArlethLB <90057862+Luz1727@users.noreply.github.com>
2025-03-12 11:55:11 -0600

    Eliminar archivos del repositorio remoto

--- alumnos/src/main/java/com/alumnos/alumnos/repository/AlumnoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alumnos.alumnos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alumnos.alumnos.model.Alumno;
-public interface AlumnoRepository extends JpaRepository <Alumno, Integer>{
-
-}
